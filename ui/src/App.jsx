5b88c2fd22df56a1866f015eaa98ca432ce16c34
KahMeng2 <kahmeng.lee12@gmail.com>
2024-08-15 20:51:27 +1000

    init

---
+++ ui/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  
+  const fetchData = async () => {
+    setLoading(true);
+    setData(null);
+    try {
+      const res = await fetch(
+        `${API_BASE_URL}/test`,
+      );
+      if (res.status > 299) {
+        setData(`woops! bad response status ${res.status} from API`);
+        return;
+      }
+      setData(await res.text());
+    } catch (e) {
+      setData(`woops! an error occurred: ${e}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        HELLO
+      </div>
+      {data && <p>{data}</p>}
+      {loading && <p>loading...</p>}
+      <button
+        disabled={loading}
+        type="button"
+        onClick={fetchData}
+      >
+        Fetch data
+      </button>
+    </>
+  );
+}
+
+export default App;
